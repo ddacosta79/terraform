6f3c6f11decd381abc47b9ee7601d70baa20e29b
ddacosta79 <ddacosta079@gmail.com>
2018-03-27 18:12:16 +0100

    new version

---
+++ BlueMix/terraform/hcl/Cloudant/mainHybrid.tf
@@ -0,0 +1,20 @@
+variable "space" {
+  description = "cloud organization space"
+}
+
+data "ibm_space" "spacedata" {
+  space = "${var.space}"
+  org   = "david.dacosta@pt.ibm.com"
+}
+
+variable "name" {
+  description = "Cloud service unique name"
+}
+
+resource "ibm_service_instance" "service_instance" {
+  name       = "${var.name}"
+  space_guid = "${data.ibm_space.spacedata.id}"
+  service    = "cloudantNoSQLDB"
+  plan       = "Lite"
+  tags       = ["cluster-service", "cluster-bind"]
+}
