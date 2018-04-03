7edb3ab8240b0388c6e1b9ddd1552b579140fdec
ddacosta79 <ddacosta079@gmail.com>
2018-04-03 18:35:03 +0100

    variables update

--- BlueMix/terraform/hcl/IaaS-simpleVM/mainHybrid.tf
+++ BlueMix/terraform/hcl/IaaS-simpleVM/mainHybrid.tf
@@ -1,9 +1,22 @@
+#########################################################
+# Define the variables
+#########################################################
+variable "datacenter" {
+  description = "Softlayer datacenter where infrastructure resources will be deployed"
+}
+
+variable "hostname" {
+  description = "Hostname of the virtual instance to be deployed"
+}
+
+
+
 # Create a virtual server
 resource "ibm_compute_vm_instance" "edpserver01" {
-hostname = "server001"
+hostname                 = "${var.hostname}"
 domain = "example.com"
 os_reference_code = "CENTOS_6_64"
-datacenter = "ams03"
+datacenter               = "${var.datacenter}"
 network_speed = 10
 cores = 1
 memory = 1024
