c3d8fc1b975d0c6144fb7ebdf0dc4518660c46bd
ddacosta79 <ddacosta079@gmail.com>
2018-04-03 13:42:58 +0100

    Novo output

--- BlueMix/terraform/hcl/Cloudant/mainHybrid.tf
+++ BlueMix/terraform/hcl/Cloudant/mainHybrid.tf
@@ -11,10 +11,29 @@ variable "name" {
   description = "Cloud service unique name"
 }
 
+#########################################################
+# resource
+#########################################################
+
 resource "ibm_service_instance" "service_instance" {
   name       = "${var.name}"
   space_guid = "${data.ibm_space.spacedata.id}"
   service    = "cloudantNoSQLDB"
   plan       = "Lite"
   tags       = ["cluster-service", "cluster-bind"]
+}
+
+#########################################################
+# Output
+#########################################################
+output "The Cloudant service ID" {
+  value = "${ibm_service_instance.id}"
+}
+
+output "The Cloudant Service Credentials" {
+  value = "${ibm_service_instance.credentials}"
+}
+
+output "The Cloudant Service Keys" {
+  value = "${ibm_service_instance.service_keys}"
 }
