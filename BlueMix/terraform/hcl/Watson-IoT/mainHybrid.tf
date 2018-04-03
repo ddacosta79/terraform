59f860d457d11e83429cfcd701daddb8dc496097
ddacosta79 <ddacosta079@gmail.com>
2018-04-03 15:46:34 +0100

    new services : IoT and Visual

--- BlueMix/terraform/hcl/Watson-IoT/mainHybrid.tf
+++ BlueMix/terraform/hcl/Watson-IoT/mainHybrid.tf
@@ -1,3 +1,7 @@
+#########################################################
+# Define the template variables
+#########################################################
+
 variable "space" {
   description = "cloud organization space"
 }
@@ -11,10 +15,29 @@ variable "name" {
   description = "Cloud service unique name"
 }
 
+#########################################################
+# resource
+#########################################################
+
 resource "ibm_service_instance" "service_instance" {
   name       = "${var.name}"
   space_guid = "${data.ibm_space.spacedata.id}"
   service    = "iotf-service"
   plan       = "iotf-service-free"
   tags       = ["cluster-service", "cluster-bind"]
+}
+
+#########################################################
+# Output
+#########################################################
+output "The IoT service ID" {
+  value = "${ibm_service_instance.service_instance.id}"
+}
+
+output "The IoT Service Credentials" {
+  value = "${ibm_service_instance.service_instance.credentials}"
+}
+
+output "The IoT Service Keys" {
+  value = "${ibm_service_instance.service_instance.service_keys}"
 }
--- BlueMix/terraform/hcl/Watson-VisualRecognition/mainHybrid.tf
+++ BlueMix/terraform/hcl/Watson-VisualRecognition/mainHybrid.tf
@@ -1,3 +1,7 @@
+#########################################################
+# Define the template variables
+#########################################################
+
 variable "space" {
   description = "cloud organization space"
 }
@@ -11,10 +15,28 @@ variable "name" {
   description = "Cloud service unique name"
 }
 
+#########################################################
+# resource
+#########################################################
+
 resource "ibm_service_instance" "service_instance" {
   name       = "${var.name}"
   space_guid = "${data.ibm_space.spacedata.id}"
   service    = "iotf-service"
   plan       = "iotf-service-free"
   tags       = ["cluster-service", "cluster-bind"]
+}
+#########################################################
+# Output
+#########################################################
+output "Watson Visual Recognition service ID" {
+  value = "${ibm_service_instance.service_instance.id}"
+}
+
+output "Watson Visual Recognition Service Credentials" {
+  value = "${ibm_service_instance.service_instance.credentials}"
+}
+
+output "Watson Visual Recognition Service Keys" {
+  value = "${ibm_service_instance.service_instance.service_keys}"
 }
